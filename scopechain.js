1aa6ad33fb6226df83a353afb13c3a4dff07734a
Kalyan Chatterjee <Kalyan.Chatterjee@cantire.com>
2019-05-20 22:16:23 -0400

    second commit

--- scopechain.js
+++ scopechain.js
@@ -10,3 +10,15 @@ function a() {
 var myVar = 1;
 
 a();
+
+// ~ 1:17:00 in the video - https://www.youtube.com/watch?v=Bv_5Zv5c-Ts
+// When you invoke a function, JavaScript creates an outer reference to the
+// outer global execution context.
+
+// This forms the scope chain. It tries to find myVar in b(). Couldn't find it,
+// so it went to the outer environment reference, which was the global execution context.
+
+// If you moved b() inside of a(), JavaScript will map the outer reference for b() to a()
+
+// Lexical structure (where the code physically sits) defines the outer environment. This is
+// known as Scope Chain.
